d2a46ce0218a1957bb4727e4446bf551885d403a
fabioachacon <fabioac71@gmail.com>
2018-08-25 16:47:39 -0300

    Add files via upload

---
+++ TestGrades.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Fábio Chacon
+ */
+public class TestGrades {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        Grades test = new Grades();
+        
+    }
+    
+}
