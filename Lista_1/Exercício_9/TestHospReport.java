18400d1b738cb5da28c9225d99811ef00d4d53f4
fabioachacon <fabioac71@gmail.com>
2018-08-28 18:42:48 -0300

    Update and rename TestHopReport.java to TestHospReport.java

---
+++ Lista_1/Exercício_9/TestHospReport.java
@@ -0,0 +1,11 @@
+
+public class TestHospReport {
+
+    public static void main(String[] args) {
+       
+        HospitalReport test = new HospitalReport();
+        test.analysis();
+        test.Report();
+    }
+    
+}
