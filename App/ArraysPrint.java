a688770ac838e53b35f5efb569110428601d0f84
fabioachacon <fabioac71@gmail.com>
2018-10-11 20:45:29 -0300

    Update ArraysPrint.java

--- App/ArraysPrint.java
+++ App/ArraysPrint.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 
 public class ArraysPrint {
     
-    private int[] array = new int[299];
+    private int[] array = new int[300];
     
     public ArraysPrint() //Construtor inicializa o array.
     {
