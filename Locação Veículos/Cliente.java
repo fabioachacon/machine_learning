ff6d7aab4e28bbe48d922d8a28052e90d8ef3981
Fábio Chacon <fabioac71@gmail.com>
2018-11-19 18:45:04 -0300

    Add files via upload

---
+++ Locação Veículos/Cliente.java
@@ -0,0 +1,31 @@
+package locadoraveiculos;
+
+public class Cliente {
+	
+	private String nome;
+	private int ID;
+	private Double CPF;
+	private String endereco;
+	private int tel;
+	
+	public void setNome(String nome) {
+		
+		this.nome = nome;
+	}
+	
+	public void setID(int ID) {
+		
+		this.ID = ID;
+	}
+	
+	public void setEndereco(String endereco) {
+		
+		this.endereco = endereco;
+	}
+	
+	public void setTel(int tel) {
+		
+		this.tel = tel;
+	}
+
+}
---
+++ Locação Veículos/Test.java
@@ -0,0 +1,16 @@
+package locadoraveiculos;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		ListaVeiculos test = new ListaVeiculos();
+        CadastroVeiculo c = new CadastroVeiculo();
+		
+		//System.out.printf(test.gerarPlaca());
+		//test.Carros();
+		 c.carrosCadastrados();
+		
+	}
+
+}
