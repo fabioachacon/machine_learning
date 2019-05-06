8fe8d8cc7c2d0e591a99bc5d167bc36a4a3c5ad5
Fábio Chacon <fabioac71@gmail.com>
2019-05-06 19:52:24 -0300

    Update HospitalReport.java

--- Lista_1/Exercício_9/HospitalReport.java
+++ Lista_1/Exercício_9/HospitalReport.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HospitalReport {
-    
       private  double weight, height;
       private  int age, total;
       private  String gender, name;
@@ -12,13 +11,9 @@ public class HospitalReport {
       private List<String> list_Wname = new ArrayList<String>();
       private List<String> listName = new ArrayList<String>();
       private List<Integer> listAge = new ArrayList<Integer>();
-      
     
-    
-     public void Report()
-    {
-     
-        System.out.printf("Quantidade de pacientes: %s\n"
+    public void Report(){
+         System.out.printf("Quantidade de pacientes: %s\n"
                 + "Média de idade dos Homens: %s\n"
                 + "Quant. de mulheres com altura entre 1,60m e 1,70m "
                 + "e peso acima de 70kg: %s\n"
@@ -26,99 +21,69 @@ public class HospitalReport {
                 + "Nome do paciente mais velho: %s\n"
                 + "Nome da mulher mais baixa: %s\n"
                 ,pcounter, getManAge(), wcounter, agecounter, olderPac(), sWoman());
-        
-        
-    }
+     }
      
-     
-    public void analysis()
-    {
+    public void analysis(){
          Scanner input = new Scanner(System.in);
-        
          System.out.printf("Forneça o nome do Paciente\n");
+         while (!("fim".equals(name = input.nextLine()))){
+             listName.add(name);
+             System.out.printf("Insira a idade: \n");
+             age = Integer.parseInt(input.nextLine()); 
+             listAge.add(age);
+             ageInterval();
         
-        while(!("fim".equals(name = input.nextLine())))
-        {
-            listName.add(name);
+             System.out.printf("Insira o peso\n");
+             weight = Double.parseDouble(input.nextLine());
             
-            System.out.printf("Insira a idade: \n");
-            age = Integer.parseInt(input.nextLine()); 
-            listAge.add(age);
-            ageInterval();
-        
-            System.out.printf("Insira o peso\n");
-            weight = Double.parseDouble(input.nextLine());
+             System.out.printf("Insira a altura\n");
+             height = Double.parseDouble(input.nextLine());
             
-            System.out.printf("Insira a altura\n");
-            height = Double.parseDouble(input.nextLine());
+             System.out.printf("Insira o sexo\n");
+             gender = input.nextLine();
             
-            System.out.printf("Insira o sexo\n");
-            gender = input.nextLine();
-            
-            if("f".equals(gender))
-            {
+            if ("f".equals(gender)) {
                 list_Wname.add(name);
                 listHeight.add(height);
                 womanCounter();
             }
-             else
-            {   
+             else{   
                 total += age; 
                 ++mcounter;
-            }
+             }
             ++pcounter;
             System.out.printf("Forneça o nome do Paciente\n");
         }
     }
     
-    public void womanCounter()
-    { 
-        if(height >= 1.60  && height <= 1.70 && weight > 70)
+    public void womanCounter(){ 
+        if (height >= 1.60  && height <= 1.70 && weight > 70)
             ++wcounter;
     }
     
-    public void ageInterval()
-    {
-        if(age >= 18 && age <= 25)
-        {
+    public void ageInterval(){
+        if (age >= 18 && age <= 25){
            ++agecounter;    
         }
     }
     
-    public String sWoman()
-    {
+    public String sWoman(){
        double min = listHeight.get(0);
-       
-       for(int j = 1; j < listHeigt.size(); j++)
-       {
-         min = Math.min(min, listHeight.get(j));
-       }
-       
+       for (int j = 1; j < listHeigt.size(); j++)
+           min = Math.min(min, listHeight.get(j));
+        
+        return list_Wname.get(listHeight.indexOf(min));
+    }
     
-     return list_Wname.get(listHeight.indexOf(min));
-     
-   }
-    
-    public String olderPac()
-    {
-       
+    public String olderPac(){
        int max = listAge.get(0);
-       
-       for(int i = 1; i < listAge.size(); i++)
-       {
-         max = Math.max(max, listAge.get(i));
-       }
-       
-    return listName.get(listAge.indexOf(max));
+       for (int i = 1; i < listAge.size(); i++)
+           max = Math.max(max, listAge.get(i));
+        
+        return listName.get(listAge.indexOf(max));
+    }
     
-   }
-    
-   
-    public double getManAge()
-   {
+    public double getManAge(){
       return total/mcounter;
    } 
-    
-    
-   
 }
